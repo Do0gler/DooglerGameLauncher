86f9c4b27d4d2fb4685adb7979c9f84a261ecf96
Elijah Ross <elijah.ross2006@gmail.com>
2024-02-17 00:47:47 -0500

    Added support for multi-file games
    Added support for games that are made up of more than one file
    Added more games

--- Scripts/GameData.gd
+++ Scripts/GameData.gd
@@ -6,6 +6,7 @@ class_name Game_Data
 @export var download_path : String
 @export var game_name : String
 @export var file_name : String
+@export var game_file_name : String
 @export_multiline var description : String
 @export var icon : Texture2D
 @export var background : Texture2D
--- Scripts/Manager.gd
+++ Scripts/Manager.gd
@@ -2,7 +2,7 @@ extends Control
 
 @export var game_panel : PackedScene
 @export var screenshot_panel : PackedScene
-var game_folder_name : String = "DooglerGamesLibrary"
+var library_folder_name : String = "DooglerGamesLibrary"
 var selected_game : Game_Data
 var current_game_pid = 0
 var game_launched = false
@@ -23,8 +23,8 @@ var http : HTTPRequest
 
 func _ready():
 	var dir = DirAccess.open("user://")
-	if(!dir.dir_exists(game_folder_name)):
-		dir.make_dir(game_folder_name)
+	if(!dir.dir_exists(library_folder_name)):
+		dir.make_dir(library_folder_name)
 		print("Created Library folder")
 	display_games()
 
@@ -70,15 +70,27 @@ func install_selected_game():
 	if selected_game != null:
 		http = HTTPRequest.new()
 		add_child(http)
-		var download_file_path = "user://" + game_folder_name + "/" + selected_game.file_name
+		# Create game folder if it doesnt exist
+		var game_folder_name = selected_game.file_name.get_basename()
+		var dir = DirAccess.open("user://" + library_folder_name)
+		if(!dir.dir_exists(game_folder_name)):
+			dir.make_dir(game_folder_name)
+		var download_file_path = "user://" + library_folder_name +\
+		 "/" + game_folder_name + "/"+ selected_game.file_name
+		# Create file to write download to
 		var new_game_file = FileAccess.open(download_file_path, FileAccess.WRITE)
+		# Start download and related functions
 		http.download_file = download_file_path
 		can_switch_games = false
 		install_button.disabled = true
 		http.request(selected_game.download_path)
 		downloading = true
 		progress_bar.show()
+		# Wait for download to finish and stop related functions
 		await http.request_completed
+		# Extract file contents if is a zip
+		if(selected_game.file_name.get_extension() == "zip"):
+			extract_zip_file(download_file_path, download_file_path.get_base_dir())
 		print("Finished Download")
 		progress_bar.hide()
 		install_button.disabled = false
@@ -93,13 +105,15 @@ func show_uninstall_confirm():
 
 func uninstall_current_game():
 	# Uninstall Game
-	var current_game_path = "user://" + game_folder_name + "/" + selected_game.file_name
-	DirAccess.remove_absolute(ProjectSettings.globalize_path(current_game_path))
+	var current_game_path = "user://" + library_folder_name +\
+		 "/" + selected_game.file_name.get_basename()
+	recursive_delete_game(current_game_path)
 	display_selected_game()
 
 func launch_selected_game():
 	if selected_game != null:
-		var current_game_path = "user://" + game_folder_name + "/" + selected_game.file_name
+		var current_game_path = "user://" + library_folder_name +\
+		 "/" + selected_game.file_name.get_basename() + "/"+ selected_game.game_file_name
 		var global_path = ProjectSettings.globalize_path(current_game_path)
 		if(selected_game.file_name.get_extension() == "exe"):
 			if current_game_pid == 0:
@@ -130,8 +144,6 @@ func display_selected_game():
 	display_name.text = selected_game.game_name
 	display_icon.texture = selected_game.icon
 	display_bg.texture = selected_game.background
-	display_description.text = "File Size: " + str(selected_game.file_size_mb) + " MB\n"\
-	 + selected_game.description
 	# Show new screenshots
 	screenshot_container.visible = !selected_game.screenshots.size() == 0
 	var i = 0
@@ -142,17 +154,60 @@ func display_selected_game():
 		screenshot.get_child(0).pressed.connect(_on_screenshot_popup_open.bind(i))
 		i += 1
 	# Show correct buttons for if file is installed or not
-	var dir = DirAccess.open("user://" + game_folder_name)
-	if dir.file_exists(selected_game.file_name):
-		install_button.hide()
-		uninstall_button.show()
+	var game_folder_path = "user://" + library_folder_name +\
+	 "/" + selected_game.file_name.get_basename()
+	var dir = DirAccess.open(game_folder_path)
+	var game_size : String
+	if dir:
+		if dir.file_exists(selected_game.game_file_name):
+			install_button.hide()
+			uninstall_button.show()
 	else:
 		install_button.show()
 		uninstall_button.hide()
+	game_size = str(selected_game.file_size_mb)
+	display_description.text = "File Size: " + game_size + " MB\n" + selected_game.description
 
 func _on_screenshot_popup_open(screenshot_index):
-	$ScreenshotPopup/VBoxContainer/TextureRect.texture = selected_game.screenshots[screenshot_index]
+	var screenshot_tex = $ScreenshotPopup/VBoxContainer/TextureRect
+	screenshot_tex.texture = selected_game.screenshots[screenshot_index]
 	$ScreenshotPopup.show()
 
 func _on_screenshot_popup_close_requested():
 	$ScreenshotPopup.hide()
+
+func extract_zip_file(file_path : String, extract_to_path : String):
+	var dir := DirAccess.open(extract_to_path)
+	var reader := ZIPReader.new()
+	var err := reader.open(file_path)
+	if err != OK:
+		print("Ecountered an error while extracting")
+	for file in reader.get_files():
+		if file.ends_with("/"):
+			continue
+		var file_base_dir = file.get_base_dir()
+		if !dir.dir_exists(file_base_dir):
+			dir.make_dir_recursive(file_base_dir)
+		var new_file_path = extract_to_path.path_join(file)
+		var new_file = FileAccess.open(new_file_path, FileAccess.WRITE)
+		new_file.store_buffer(reader.read_file(file))
+	reader.close()
+	# Wait for the zip file to close
+	await get_tree().create_timer(0.1).timeout
+	dir.remove(selected_game.file_name)
+	print("Done Extracting")
+
+func recursive_delete_game(dirPath):
+	var dir = DirAccess.open(dirPath)
+	dir.list_dir_begin()
+	var fileName = dir.get_next()
+	while fileName != "":
+		var filePath = dirPath + "/" + fileName
+		if dir.current_is_dir():
+			recursive_delete_game(filePath)
+		else:
+			print("Deleting: " + filePath)
+			DirAccess.remove_absolute(filePath)
+		fileName = dir.get_next()
+	dir.list_dir_end()
+	DirAccess.remove_absolute(dirPath)
